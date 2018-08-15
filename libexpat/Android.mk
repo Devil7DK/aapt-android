bab86e40a54304fa457efc589908ef02182dd47e
Devil7DK <dineshthangavel47@gmail.com>
2018-08-15 16:54:12 +0530

    Cleanup libexpat

--- libexpat/Android.mk
+++ libexpat/Android.mk
@@ -1,8 +1,5 @@
 LOCAL_PATH:= $(call my-dir)
 
-# We need to build this for both the device (as a shared library)
-# and the host (as a static library for tools to use).
-
 common_SRC_FILES := \
 	lib/xmlparse.c \
 	lib/xmlrole.c \
@@ -18,45 +15,6 @@ common_CFLAGS := \
 common_C_INCLUDES += \
 	$(LOCAL_PATH)/lib
 
-# For the host
-# =====================================================
-
-# Host static library
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := $(common_SRC_FILES)
-LOCAL_CFLAGS += $(common_CFLAGS)
-LOCAL_C_INCLUDES += $(common_C_INCLUDES)
-
-LOCAL_CFLAGS_darwin += -fno-common
-
-LOCAL_MODULE:= libexpat
-LOCAL_MODULE_HOST_OS := darwin linux windows
-LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/lib
-
-LOCAL_MULTILIB := both
-
-include $(BUILD_HOST_STATIC_LIBRARY)
-
-# Host shared library
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := $(common_SRC_FILES)
-LOCAL_CFLAGS += $(common_CFLAGS)
-LOCAL_C_INCLUDES += $(common_C_INCLUDES)
-
-LOCAL_CFLAGS_darwin += -fno-common
-
-LOCAL_MODULE:= libexpat-host
-LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/lib
-LOCAL_MULTILIB := both
-
-include $(BUILD_HOST_SHARED_LIBRARY)
-
-
-# For the device
-# =====================================================
-
 # Device static library
 include $(CLEAR_VARS)
 
@@ -70,7 +28,7 @@ LOCAL_SRC_FILES := $(common_SRC_FILES)
 LOCAL_CFLAGS += $(common_CFLAGS)
 LOCAL_C_INCLUDES += $(common_C_INCLUDES)
 
-LOCAL_MODULE:= libexpat_static
+LOCAL_MODULE:= libexpat
 LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
 LOCAL_MODULE_TAGS := optional
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/lib
